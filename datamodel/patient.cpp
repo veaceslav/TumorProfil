92ff8236f2bc27f600334b3943a89902fe6edc7f
Marcel Wiesweg <marcel.wiesweg@uk-essen.de>
2013-02-25 00:25:25 +0100

    safety debug statement

--- datamodel/patient.cpp
+++ datamodel/patient.cpp
@@ -19,6 +19,8 @@
  *
  * ============================================================ */
 
+#include <QDebug>
+
 #include "patient.h"
 
 Patient::Patient()
@@ -45,6 +47,10 @@ bool Patient::hasDisease() const
 }
 Disease& Patient::firstDisease()
 {
+    if (diseases.isEmpty())
+    {
+        qDebug() << "Expect crash";
+    }
     return diseases.first();
 }
 
