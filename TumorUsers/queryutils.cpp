7226337f251e986240deac58aea27cd649166949
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2016-01-02 17:32:19 +0100

    Add special symbols to random key generator

--- TumorUsers/queryutils.cpp
+++ TumorUsers/queryutils.cpp
@@ -98,7 +98,7 @@ UserDetails QueryUtils::editUser(QString name, QueryUtils::UserType userType, QS
 QString QueryUtils::generateRandomString(int length)
 {
     qsrand(time(NULL));
-    QString base("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+    QString base("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()[]{}:;|<>?/+-=");
     QString str;
     for(int i=0;i<length;i++){
         str += base.at(qrand() % base.length());
